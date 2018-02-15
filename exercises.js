edea50c1c7ef949a0df0f443a4dcda3a0d93385d
Reese Akamine <Arresayy@gmail.com>
2018-02-14 23:20:42 -1000

    complete exercise 1

--- exercises.js
+++ exercises.js
@@ -13,6 +13,15 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
     
     Console.log the object.
 */
+
+var books = {
+  title: "The Obstacle is the Way",
+  author: "Ryan Holiday",
+  category: "Self Help",
+  pages: 200,
+};
+
+console.log(books);
     
   
 /*
