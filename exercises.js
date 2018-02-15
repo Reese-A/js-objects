70885212dea126e9ff10d04f7153077528ab977c
Reese Akamine <Arresayy@gmail.com>
2018-02-15 00:07:33 -1000

    commit exercise 4

--- exercises.js
+++ exercises.js
@@ -85,6 +85,12 @@ console.log(kicks);
   console.log the object
 */
 
+var plainBox = {};
+plainBox.color = "green";
+plainBox.size = 16;
+plainBox.contents = [];
+
+console.log(plainBox);
 
 /*
 5. Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
