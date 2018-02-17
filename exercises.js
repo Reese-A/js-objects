7c9cb0fa0e3466681685d04e038aa3cb78ce675a
Reese Akamine <Arresayy@gmail.com>
2018-02-16 18:58:19 -1000

    complete exercise 10

--- exercises.js
+++ exercises.js
@@ -18,7 +18,7 @@ var books = {
   title: "The Obstacle is the Way",
   author: "Ryan Holiday",
   category: "Self Help",
-  pages: 200,
+  pages: 200
 };
 
 console.log(books);
@@ -292,6 +292,16 @@ console.log(sumObj);
         plainBoxResult and use `console.log` to inspect your results.
  */
 
+function putInPlainBox(obj){
+  for(var i = 0; i < 10; i++){
+    var randomNum = Math.floor(Math.random() * 10);
+    obj.contents.push(randomNum);
+  }
+  return obj;
+}
+
+var plainBoxResult = putInPlainBox(plainBox);
+console.log(plainBoxResult);
 
 /*
 11. Detecting transmission
